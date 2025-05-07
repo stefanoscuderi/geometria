b4157f09c32d85e8bf65611c4d007488d900f107
Mayumi Shingaki <mayu.shingaki@gmail.com>
2025-05-07 00:48:11 -0300

    build: Adicionar encoding e atualizar versão do setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="geometria",
-    version="0.2",
+    version="0.5",
     author="Mayumi Shingaki",
     description="Biblioteca para formulas de geometria",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(include=["geometria"]),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -19,6 +19,5 @@ setup(
     python_requires='>=3.8',
     install_requires=[],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'],
-    dependency_links=['https://github.com/mayumisngk/school_math']
+    tests_require=['pytest==4.4.1']
 )
